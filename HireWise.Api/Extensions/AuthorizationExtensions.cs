afff065911b1bf49b44c33e28fc68bb55d80c438
DanGritsyuk <Denis@Gritsyuk.ru>
2024-09-16 21:27:02 +0300

    Добавил авторизацию в сваггер

--- HireWise.Api/Extensions/AuthorizationExtensions.cs
+++ HireWise.Api/Extensions/AuthorizationExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using HireWise.BLL.Logic.Authorization;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 namespace HireWise.Api.Extensions
 {
@@ -46,6 +47,41 @@ namespace HireWise.Api.Extensions
                         return Task.CompletedTask;
                     }
                 };
+            });
+
+            services.AddSwaggerGen(swagger =>
+            {
+                //This is to generate the Default UI of Swagger Documentation
+                swagger.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "JWT Token Authentication API",
+                    Description = "ASP.NET Core 5.0 Web API"
+                });
+                // To Enable authorization using Swagger (JWT)
+                swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                });
+                swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                            },
+                            new string[] {}
+                    }
+                });
             });
 
             return services;
